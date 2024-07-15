d811e1d790fb8c197378bdf5b0b8820d40226dcc
junkf8d <93854615+junkf8d@users.noreply.github.com>
2024-07-16 01:57:50 +0900

    aboutページを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   get 'foo/baz'
   get 'static_pages/home'
   get 'static_pages/help'
+  get 'static_pages/about'
+
+
+
   resources :microposts
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
